14f8b4bda30403bc253c48d776db70f7827cf1e5
Jakub Leško <jakub.lesko@outlook.com>
2020-11-04 14:31:55 +0100

    Add image preview modal component

--- client/blog.js
+++ client/blog.js
@@ -1,17 +1,42 @@
 import './blog.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const openMenuBtn = document.querySelector('#menu-open-btn');
-  const closeMenuBtn = document.querySelector('#menu-close-btn');
   const menuContainer = document.querySelector('#menu-container');
+  const menuOpenBtn = document.querySelector('#menu-open-btn');
+  const menuCloseBtn = document.querySelector('#menu-close-btn');
 
-  openMenuBtn?.addEventListener('click', () => {
+  menuOpenBtn?.addEventListener('click', () => {
     menuContainer?.classList.remove('hidden');
     menuContainer?.classList.add('absolute');
   });
 
-  closeMenuBtn?.addEventListener('click', () => {
+  menuCloseBtn?.addEventListener('click', () => {
     menuContainer?.classList.remove('absolute');
     menuContainer?.classList.add('hidden');
+  });
+
+  const previewContainer = document.querySelector('#preview-container');
+  const previewBackdrop = document.querySelector('#preview-backdrop');
+  const previewCloseBtn = document.querySelector('#preview-close');
+  const previewElement = document.querySelector('#preview-image');
+
+  [...document.querySelectorAll('img')]
+    .filter((image) => image.getAttribute('src') !== null)
+    .forEach((image) => image.addEventListener('click', () => {
+      previewElement?.setAttribute('src', image.getAttribute('src'));
+      previewElement?.setAttribute('alt', image.getAttribute('alt'));
+
+      previewContainer?.classList.remove('hidden');
+      previewContainer?.classList.add('fixed');
+    }));
+
+  previewBackdrop?.addEventListener('click', () => {
+    previewContainer?.classList.remove('fixed');
+    previewContainer?.classList.add('hidden');
+  });
+
+  previewCloseBtn?.addEventListener('click', () => {
+    previewContainer?.classList.remove('fixed');
+    previewContainer?.classList.add('hidden');
   });
 });
