8cb61e5a5e9ef7a4f953fcd76105b0461dadf1eb
Jakub Leško <mail@jakublesko.com>
2022-07-27 21:26:30 +0200

    Add NextJS application skeleton

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Jakub Leško</title>
+      </Head>
+
+      <p>It works!</p>
+    </Fragment>
+  );
+}
